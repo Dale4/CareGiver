9f3d9bb25a2020ebf34327aa3478d040e4a1d1bb
Dale4 <dale_oneill@hotmail.com>
2025-03-22 16:23:51 -0700

    Add source directory

---
+++ source/GenerateERData/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello from .NET 8 Minimal API running on ECS Fargate!");
+
+app.Run();
+
