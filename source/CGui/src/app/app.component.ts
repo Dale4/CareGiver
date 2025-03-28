f0c44a142e4826530dcacfbb11588cbb7f34e2e7
Dale4 <dale_oneill@hotmail.com>
2025-03-28 14:44:46 -0700

    Removed old code

--- source/CGui/src/app/app.component.ts
+++ source/CGui/src/app/app.component.ts
@@ -1,27 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { HeaderComponent } from "./header/header.component";
-import { SettingsComponent } from './settings/settings.component';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SettingsComponent, MatIconModule],
+  imports: [RouterOutlet, HeaderComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = '';
 
-  @ViewChild('slider') slider: SettingsComponent | undefined;
-  
   constructor(private titleService: Title) {
     this.titleService.setTitle('CareGiver');
     this.title = this.titleService.getTitle();
   }
-
- 
   
 }
